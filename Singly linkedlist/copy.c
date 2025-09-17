452694d99dcda363f0542af227235d423a7c9826
Panchal Vedantkumar Niravbhai <panchalvedant.7775@gmail.com>
2025-09-17 10:11:08 +0530

    Add files via upload

---
+++ Singly linkedlist/copy.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int info;
+    struct Node* link;
+};
+
+
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d->", temp->info);
+        temp = temp->link;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node*)malloc(sizeof(struct Node));
+
+    head->info = 10;
+    head->link = second;
+
+    second->info = 20;
+    second->link = third;
+
+    third->info = 30;
+    third->link = NULL;
+
+    printf("Original List: ");
+    printList(head);
+
+    
+    struct Node* original = head;
+    struct Node* copyHead = NULL;
+    struct Node* copyTail = NULL;
+
+    while (original != NULL) {
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->info = original->info;
+        newNode->link = NULL;
+
+        if (copyHead == NULL) {
+            copyHead = newNode;
+            copyTail = newNode;
+        } else {
+            copyTail->link = newNode;
+            copyTail = newNode;
+        }
+
+        original = original->link;
+    }
+
+    printf("Copied List: ");
+    printList(copyHead);
+
+  
+    free(head);
+    free(second);
+    free(third);
+
+   
+    struct Node* temp;
+    while (copyHead != NULL) {
+        temp = copyHead;
+        copyHead = copyHead->link;
+        free(temp);
+    }
+
+    return 0;
+}
